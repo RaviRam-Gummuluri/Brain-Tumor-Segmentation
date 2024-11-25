7e99417f71126f3494ff39dc8ccff11b2905589a
Ashinee-20 <100423063+Ashinee-20@users.noreply.github.com>
2024-11-25 21:20:18 +0530

    Update Login.js

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -17,7 +17,7 @@ function Login({ onLogin }) {
     }
 
     try {
-      const response = await fetch('http://localhost:5000/login', {
+      const response = await fetch('https://brain-tumor-segmentation-cfzs.onrender.com/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
